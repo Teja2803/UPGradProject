fe5c61cd7ec35e05e958f0b72e2e94ca3e6a4664
Teja2803 <tejaswini055@gmail.com>
2025-06-08 12:05:22 +0530

    provider terraform

---
+++ Task1/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "teja2803-terraform-bucket"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
