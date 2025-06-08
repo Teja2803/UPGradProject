abc30172da7d76be19ec7384fcdeed4ecc1351b8
Teja2803 <tejaswini055@gmail.com>
2025-06-08 12:30:22 +0530

    added azs2

--- Task1/variable.tf
+++ Task1/variable.tf
@@ -5,18 +5,18 @@ variable "cidr_block" {
 }
 variable "azs" {
     type = list
-    default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+    default = ["ap-south-1a", "ap-south-1b"]
     description = "AZs"
 }
 
 variable "private_subnets" {
     type = list
-    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
     description = "private subnets"
 } 
 
 variable "public_subnets" {
     type = list
-    default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    default = ["10.0.101.0/24", "10.0.102.0/24"]
     description = "public subnet"
 } 
