5e1aa4b68dd4b4dcd4411e5c4e16ac4a24402f66
Teja2803 <tejaswini055@gmail.com>
2025-06-08 12:19:25 +0530

    added variable and network files

---
+++ Task1/variable.tf
@@ -0,0 +1,22 @@
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+    description = "cidr for VPC"
+    type = string
+}
+variable "azs" {
+    type = list
+    default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+    description = "AZs"
+}
+
+variable "private_subnets" {
+    type = list
+    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    description = "private subnets"
+} 
+
+variable "public_subnets" {
+    type = list
+    default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    description = "public subnet"
+} 
