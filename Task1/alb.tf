67d43f8d228cbf4b00159553628a7d6eb8c63cea
Teja2803 <tejaswini055@gmail.com>
2025-06-12 12:59:57 +0530

    alb

---
+++ Task1/alb.tf
@@ -0,0 +1,65 @@
+resource "aws_lb" "my_alb" {
+ internal           = false
+ load_balancer_type = "application"
+ security_groups    = [aws_security_group.alb_sg.id]
+ subnets            = [aws_subnet.private["1"].id, aws_subnet.private["0"].id]
+
+ tags = {
+   Name = "${var.env}-alb"
+ }
+}
+
+resource "aws_lb_target_group" "alb_tg" {
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.env}-tg"
+  }
+}
+
+
+resource "aws_lb_target_group_attachment" "tg_attach" {
+  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_id        = aws_instance.private_ec2_1.id
+  port             = 8080
+}
+
+resource "aws_lb_listener" "alb_list" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "jenkins" {
+  listener_arn = aws_lb_listener.alb_list.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/jenkins*"]
+    }
+  }
+}
+
--- Task1/sg.tf
+++ Task1/sg.tf
@@ -51,3 +51,27 @@ resource "aws_security_group" "sg_vpc_only" {
     Name = "${var.env}-sg-vpc"
   }
 }
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-alb-sg"
+  }
+}
