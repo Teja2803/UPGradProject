f22fb413992edc13731e42ceb37f4dd1b5eaf12a
Teja2803 <tejaswini055@gmail.com>
2025-06-12 14:12:06 +0530

    alb

--- Task1/alb.tf
+++ Task1/alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "my_alb" {
  internal           = false
  load_balancer_type = "application"
  security_groups    = [aws_security_group.alb_sg.id]
- subnets            = [aws_subnet.private["1"].id, aws_subnet.private["0"].id]
+ subnets            = [aws_subnet.public["1"].id, aws_subnet.public["0"].id]
 
  tags = {
    Name = "${var.env}-alb"
