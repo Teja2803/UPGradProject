cbf799c574deadbe5578eed4938d03548dbcbbc6
Teja2803 <tejaswini055@gmail.com>
2025-06-12 18:54:49 +0530

    alb

--- Task1/alb.tf
+++ Task1/alb.tf
@@ -29,6 +29,41 @@ resource "aws_lb_target_group" "alb_tg" {
   }
 }
 
+resource "aws_lb_target_group" "alb_tg2" {
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.env}-tg"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tg_attach2" {
+  target_group_arn = aws_lb_target_group.alb_tg2.arn
+  target_id        = aws_instance.private_ec2_2.id
+  port             = 80
+}
+resource "aws_lb_listener" "alb_list" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg2.arn
+  }
+}
 
 resource "aws_lb_target_group_attachment" "tg_attach" {
   target_group_arn = aws_lb_target_group.alb_tg.arn
