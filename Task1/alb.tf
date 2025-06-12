2b73a1dac6adec8b85b0d2a585918bdfd1be9dda
Teja2803 <tejaswini055@gmail.com>
2025-06-12 18:59:09 +0530

    alb

--- Task1/alb.tf
+++ Task1/alb.tf
@@ -30,7 +30,7 @@ resource "aws_lb_target_group" "alb_tg" {
 }
 
 resource "aws_lb_target_group" "alb_tg2" {
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
@@ -52,11 +52,11 @@ resource "aws_lb_target_group" "alb_tg2" {
 resource "aws_lb_target_group_attachment" "tg_attach2" {
   target_group_arn = aws_lb_target_group.alb_tg2.arn
   target_id        = aws_instance.private_ec2_2.id
-  port             = 80
+  port             = 8080
 }
-resource "aws_lb_listener" "alb_list" {
+resource "aws_lb_listener" "alb_list2" {
   load_balancer_arn = aws_lb.my_alb.arn
-  port              = 80
+  port              = 8080
   protocol          = "HTTP"
 
   default_action {
@@ -98,3 +98,18 @@ resource "aws_lb_listener_rule" "jenkins" {
   }
 }
 
+esource "aws_lb_listener_rule" "app" {
+  listener_arn = aws_lb_listener.alb_list2.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg2.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/app*"]
+    }
+  }
+}
